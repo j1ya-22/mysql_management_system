38a4f280dd06c83e5193e055d5374712b35b235a
qyj <2254466875@qq.com>
2023-12-07 14:25:27 +0800

    qetx commit

--- db_control.py
+++ db_control.py
@@ -79,7 +79,7 @@ class redgister_db(object):
     def add_T(self,Pid,Pname,Rid,Pon,Pstime,Petime,Pvolume,Plight):
         sql1="INSERT INTO Product VALUES (%s,%s,%s,%s);"
         sql2="INSERT INTO Television VALUES (%s, %s, %s, %s, %s, %s);"
-        sql3="SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s "
+        sql3="SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s;"
         pstyle="电视机"
         with self.cursor as cursor:
             data_origin=cursor.execute(sql3, (Pname, Rid,))
@@ -92,35 +92,55 @@ class redgister_db(object):
                 return "添加成功"
 
 
-    def add_C(self,Pid,Cname,Rid,Pon,Pstime,Petime,Plight):
+    def add_C(self,Pid,Pname,Rid,Pon,Pstime,Petime,Plight):
         sql1 = "INSERT INTO Product VALUES (%s, %s, %s, %s);"
         sql2 = "INSERT INTO Curtain VALUES (%s, %s, %s, %s, %s);"
+        sql3 = "SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s;"
         pstyle = "窗帘"
         with self.cursor as cursor:
-            cursor.execute(sql1, (Pid, Cname, pstyle, Rid,))
-            cursor.execute(sql2, (Pid, Pon, Pstime, Petime, Plight,))
-            self.connect.commit()
+            data_origin = cursor.execute(sql3, (Pname, Rid,))
+            if data_origin:
+                return "该窗帘已经在该房间存在"
+            else:
+                cursor.execute(sql1, (Pid, Pname, pstyle, Rid,))
+                cursor.execute(sql2, (Pid, Pon, Pstime, Petime, Plight,))
+                self.connect.commit()
+                return "添加成功"
 
 
-    def add_A(self,Pid,Aname,Rid,Pon,Pstime,Petime,Ptemperature,Pwet):
+    def add_A(self,Pid,Pname,Rid,Pon,Pstime,Petime,Ptemperature,Pwet):
         sql1 = "INSERT INTO Product VALUES (%s, %s, %s, %s);"
-        sql2 = "INSERT INTO Curtain VALUES (%s, %s, %s, %s, %s);"
+        sql2 = "INSERT INTO AirConditioner VALUES (%s, %s, %s, %s, %s, %s);"
+        sql3 = "SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s;"
         pstyle = "空调"
         with self.cursor as cursor:
-            cursor.execute(sql1, (Pid, Aname, pstyle, Rid,))
-            cursor.execute(sql2, (Pid, Pon, Pstime, Petime, Ptemperature,Pwet,))
-            self.connect.commit()
+            data_origin = cursor.execute(sql3, (Pname, Rid,))
+            if data_origin:
+                return "该空调已经在该房间存在"
+            else:
+                cursor.execute(sql1, (Pid, Pname, pstyle, Rid,))
+                cursor.execute(sql2, (Pid, Pon, Pstime, Petime, Ptemperature,Pwet,))
+                self.connect.commit()
+                return "添加成功"
 
 
 
-    def add_L(self,Pid,Aname,Rid,Pon,Pstime,Petime,Plight,Pcolor):
+    def add_L(self,Pid,Pname,Rid,Pon,Pstime,Petime,Plight,Pcolor):
         sql1 = "INSERT INTO Product VALUES (%s, %s, %s, %s);"
-        sql2 = "INSERT INTO Curtain VALUES (%s, %s, %s, %s, %s);"
+        sql2 = "INSERT INTO Light VALUES (%s, %s, %s, %s, %s,%s);"
+        sql3 = "SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s;"
         pstyle = "电灯"
         with self.cursor as cursor:
-            cursor.execute(sql1, (Pid, Aname, pstyle, Rid,))
-            cursor.execute(sql2, (Pid, Pon, Pstime, Petime, Plight,Pcolor,))
-            self.connect.commit()
+            data_origin = cursor.execute(sql3, (Pname, Rid,))
+            if data_origin:
+                return "该电灯已经在该房间存在"
+            else:
+                cursor.execute(sql1, (Pid, Pname, pstyle, Rid,))
+                cursor.execute(sql2, (Pid, Pon, Pstime, Petime, Plight,Pcolor,))
+                self.connect.commit()
+                return "添加成功"
+
+
     def del_T(self,Tname,Rid):
         sql1="SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s"
         with self.cursor as cursor:
@@ -128,11 +148,63 @@ class redgister_db(object):
             data_origin = cursor.fetchall()
             if data_origin:
                 Pid=data_origin[0][0]
-                sql2 = "DELETE  FROM Product WHERE Pid=%s"
-                sql3 = "DELETE  FROM Television WHERE Pid=%s"
+                sql2 = "DELETE  FROM Product WHERE Pid=%s;"
+                sql3 = "DELETE  FROM Television WHERE Pid=%s;"
                 cursor.execute(sql2, (Pid,))
                 cursor.execute(sql3, (Pid,))
                 self.connect.commit()
                 return "删除成功"
             else:
                 return "没有这个电视哦"
+
+
+    def del_C(self,Cname,Rid):
+        sql1="SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s"
+        with self.cursor as cursor:
+            cursor.execute(sql1, (Cname,Rid))
+            data_origin = cursor.fetchall()
+            if data_origin:
+                Pid=data_origin[0][0]
+                sql2 = "DELETE  FROM Product WHERE Pid=%s;"
+                sql3 = "DELETE  FROM Curtain WHERE Pid=%s;"
+                cursor.execute(sql2, (Pid,))
+                cursor.execute(sql3, (Pid,))
+                self.connect.commit()
+                return "删除成功"
+            else:
+                return "没有这个窗帘哦"
+
+
+    def del_A(self,Aname,Rid):
+        sql1="SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s"
+        with self.cursor as cursor:
+            cursor.execute(sql1, (Aname,Rid))
+            data_origin = cursor.fetchall()
+            if data_origin:
+                Pid=data_origin[0][0]
+                print(Pid)
+                sql2 = "DELETE  FROM Product WHERE Pid=%s;"
+                sql3 = "DELETE  FROM AirConditioner WHERE Pid=%s;"
+                cursor.execute(sql2, (Pid,))
+                cursor.execute(sql3, (Pid,))
+                self.connect.commit()
+                return "删除成功"
+            else:
+                return "没有这个空调哦"
+
+    def del_L(self, Lname, Rid):
+        sql1 = "SELECT Pid FROM Product WHERE Pname=%s AND Rid=%s"
+        with self.cursor as cursor:
+            cursor.execute(sql1, (Lname, Rid))
+            data_origin = cursor.fetchall()
+            if data_origin:
+                Pid = data_origin[0][0]
+
+                sql2 = "DELETE  FROM Product WHERE Pid=%s;"
+                sql3 = "DELETE  FROM Light WHERE Pid=%s;"
+                cursor.execute(sql2, (Pid,))
+                cursor.execute(sql3, (Pid,))
+                self.connect.commit()
+                return "删除成功"
+            else:
+                return "没有这个电灯哦"
--- form_1.py
+++ form_1.py
@@ -11,6 +11,7 @@ app.config['JSON_AS_ASCII'] = False
 app.secret_key = secrets.token_hex(16)  # 对用户信息加密,随机生成密钥更加安全
 print(app.secret_key)
 
+
 def md5_hash(text):
     text = text.encode('utf-8')
     md5_encode = md5(text)
@@ -44,13 +45,13 @@ def login():
         if data:
             if md5_hash(password) == data[0][1] and username == data[0][0]:  # 将密码和数据库中存放的密码进行比对
                 session['user_info'] = username  # 生成session，后期可以进行鉴权
-                return jsonify({'msg': "登录成功","redirectUrl":"/header"})
+                return jsonify({'msg': "登录成功", "redirectUrl": "/header"})
             else:
-                return jsonify({'msg': "密码错误","redirectUrl":"/"})
+                return jsonify({'msg': "密码错误", "redirectUrl": "/"})
         else:
-            return jsonify({'msg': "用户名不存在","redirectUrl":"/"})
+            return jsonify({'msg': "用户名不存在", "redirectUrl": "/"})
     else:
-        return jsonify({'msg': "用户名或密码不能为空哈","redirectUrl":"/"})
+        return jsonify({'msg': "用户名或密码不能为空哈", "redirectUrl": "/"})
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -63,34 +64,41 @@ def logup():
     token = data['token']
     verify = t.signin('admin')
     data = r.signin(user)
-    if verify[0][1]==md5_hash(token):
+    if verify[0][1] == md5_hash(token):
         if data:
-            return jsonify({'msg': "用户名已存在","redirectUrl":"/"})
+            return jsonify({'msg': "用户名已存在", "redirectUrl": "/"})
         else:
             r = redgister_db()
             pwd = md5_hash(pwd)
             r.signup(user, pwd)
-            return jsonify({'msg': "注册成功，请去登录","redirectUrl":"/"})
+            return jsonify({'msg': "注册成功，请去登录", "redirectUrl": "/"})
     else:
         return jsonify({'msg': "token错误", "redirectUrl": "/"})
+
+
 @app.route('/header', methods=['GET', 'POST'])
 @login_required
 def home_page():
     if request.method == 'GET':
         return render_template('header.html')
 
+
 @app.route('/select_T', methods=['GET', 'POST'])
 @login_required
 def select_T():
     r = redgister_db()
-    data_all=r.select_Television()
-    return render_template('list_Television.html',data_all=data_all,data_num=len(data_all))
+    data_all = r.select_Television()
+    return render_template('list_Television.html', data_all=data_all, data_num=len(data_all))
+
+
 @app.route('/select_A', methods=['GET', 'POST'])
 @login_required
 def select_A():
     r = redgister_db()
-    data_all=r.select_Air()
-    return render_template('list_Aircondition.html',data_all=data_all,data_num=len(data_all))
+    data_all = r.select_Air()
+    return render_template('list_Aircondition.html', data_all=data_all, data_num=len(data_all))
+
+
 @app.route('/select_C', methods=['GET', 'POST'])
 @login_required
 def select_C():
@@ -101,8 +109,10 @@ def select_C():
 @login_required
 def select_L():
     r = redgister_db()
-    data_all=r.select_Light()
-    return render_template('list_Light.html',data_all=data_all,data_num=len(data_all))
+    data_all = r.select_Light()
+    return render_template('list_Light.html', data_all=data_all, data_num=len(data_all))
+
+
 @app.route('/add_T', methods=['GET', 'POST'])
 @login_required
 def add_T():
@@ -115,7 +125,7 @@ def create_T():
     r = redgister_db()
     t = redgister_db()
     g = redgister_db()
-    max_pid=r.select_maxT()
+    max_pid = r.select_maxT()
     Pid = max_pid[:1] + str(int(max_pid[1:]) + 1).zfill(len(max_pid) - 1)
     Tname = data['p_name']
     Rid = int(t.select_Rid(data['p_room']))
@@ -135,9 +145,87 @@ def create_T():
 def add_C():
     return render_template('create_curtain.html')
 
+@app.route('/create_C', methods=['GET', 'POST'])
+@login_required
+def create_C():
+    data = json.loads(request.data)
+    r = redgister_db()
+    t = redgister_db()
+    g = redgister_db()
+    max_pid=r.select_maxC()
+    Pid = max_pid[:1] + str(int(max_pid[1:]) + 1).zfill(len(max_pid) - 1)
+    Tname = data['p_name']
+    Rid = int(t.select_Rid(data['p_room']))
+    Pon = data['p_on']
+    Pstime = data['p_stime']
+    Petime = data['p_etime']
+    Plight = data['p_light']
+    massage=g.add_C(Pid,Tname,Rid,Pon,Pstime,Petime,Plight)
+    if massage=="添加成功":
+        return jsonify({'msg': massage, "redirectUrl": "/select_C"})
+    else:
+        return jsonify({'msg': massage, "redirectUrl": "/add_C"})
 
 
 
+
+@app.route('/add_A', methods=['GET', 'POST'])
+@login_required
+def add_A():
+    return render_template('create_airconditioner.html')
+
+@app.route('/create_A', methods=['GET', 'POST'])
+@login_required
+def create_A():
+    data = json.loads(request.data)
+    r = redgister_db()
+    t = redgister_db()
+    g = redgister_db()
+    max_pid=r.select_maxA()
+    Pid = max_pid[:1] + str(int(max_pid[1:]) + 1).zfill(len(max_pid) - 1)
+    Tname = data['p_name']
+    Rid = int(t.select_Rid(data['p_room']))
+    Pon = data['p_on']
+    Pstime = data['p_stime']
+    Petime = data['p_etime']
+    Ptemperature = data['p_temperature']
+    Pwet = data['p_wet']
+    massage=g.add_A(Pid,Tname,Rid,Pon,Pstime,Petime,Ptemperature,Pwet)
+    if massage=="添加成功":
+        return jsonify({'msg': massage, "redirectUrl": "/select_A"})
+    else:
+        return jsonify({'msg': massage, "redirectUrl": "/add_A"})
+
+
+
+@app.route('/add_L', methods=['GET', 'POST'])
+@login_required
+def add_L():
+    return render_template('create_light.html')
+
+@app.route('/create_L', methods=['GET', 'POST'])
+@login_required
+def create_L():
+    data = json.loads(request.data)
+    r = redgister_db()
+    t = redgister_db()
+    g = redgister_db()
+    max_pid=r.select_maxL()
+    Pid = max_pid[:1] + str(int(max_pid[1:]) + 1).zfill(len(max_pid) - 1)
+    Tname = data['p_name']
+    Rid = int(t.select_Rid(data['p_room']))
+    Pon = data['p_on']
+    Pstime = data['p_stime']
+    Petime = data['p_etime']
+    Plight = data['p_light']
+    Pcolor = data['p_color']
+    print(Pid,Tname,Rid,Pon,Pstime,Petime,Plight,Pcolor)
+    massage=g.add_L(Pid,Tname,Rid,Pon,Pstime,Petime,Plight,Pcolor)
+    print(massage)
+    if massage=="添加成功":
+        return jsonify({'msg': massage, "redirectUrl": "/select_L"})
+    else:
+        return jsonify({'msg': massage, "redirectUrl": "/add_L"})
 
 
 
@@ -161,6 +249,71 @@ def delete_T():
         return jsonify({'msg': massage, "redirectUrl": "/select_T"})
     else:
         return jsonify({'msg': massage, "redirectUrl": "/del_T"})
+
+
+@app.route('/del_C', methods=['GET', 'POST'])
+@login_required
+def del_C():
+    return render_template('delete_curtain.html')
+
+@app.route('/delete_C', methods=['GET', 'POST'])
+@login_required
+def delete_C():
+    data = json.loads(request.data)
+    Cname=data['p_name']
+    Rname = data['p_room']
+    r = redgister_db()
+    t = redgister_db()
+    Rid = t.select_Rid(Rname)
+    massage = r.del_C(Cname,Rid)
+    if massage=="删除成功":
+        return jsonify({'msg': massage, "redirectUrl": "/select_C"})
+    else:
+        return jsonify({'msg': massage, "redirectUrl": "/del_C"})
+
+
+
+@app.route('/del_A', methods=['GET', 'POST'])
+@login_required
+def del_A():
+    return render_template('delete_airconditioner.html')
+
+@app.route('/delete_A', methods=['GET', 'POST'])
+@login_required
+def delete_A():
+    data = json.loads(request.data)
+    Aname=data['p_name']
+    Rname = data['p_room']
+    r = redgister_db()
+    t = redgister_db()
+    Rid = t.select_Rid(Rname)
+    massage = r.del_A(Aname,Rid)
+    if massage=="删除成功":
+        return jsonify({'msg': massage, "redirectUrl": "/select_A"})
+    else:
+        return jsonify({'msg': massage, "redirectUrl": "/del_A"})
+
+
+
+@app.route('/del_L', methods=['GET', 'POST'])
+@login_required
+def del_L():
+    return render_template('delete_light.html')
+
+@app.route('/delete_L', methods=['GET', 'POST'])
+@login_required
+def delete_L():
+    data = json.loads(request.data)
+    Lname = data['p_name']
+    Rname = data['p_room']
+    r = redgister_db()
+    t = redgister_db()
+    Rid = t.select_Rid(Rname)
+    massage = r.del_L(Lname,Rid)
+    if massage=="删除成功":
+        return jsonify({'msg': massage, "redirectUrl": "/select_L"})
+    else:
+        return jsonify({'msg': massage, "redirectUrl": "/del_L"})
 
 
 
